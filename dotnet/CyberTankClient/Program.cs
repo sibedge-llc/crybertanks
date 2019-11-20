73c8cffa3639e79cc604a9a6f4b87dc53c7bbf79
Konstantin <bederkb@sibedge.com>
2019-11-20 13:01:25 +0700

    Add boilerplate for dotnet

---
+++ dotnet/CyberTankClient/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CyberTankClient
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serverUrl = "https://cybertank.sibedge.com:5001/gameHub";
+
+            using (var bot = new CyberTankBot(serverUrl, GameMode.Debug, "Player_1"))
+            {
+                bot.OnReceiveMessage += message => { Console.WriteLine(message); };
+                bot.Start();
+
+                Console.Read();
+            }
+        }
+    }
+}
